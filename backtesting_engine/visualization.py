81bee46881d2b2674a830798a9e096fa6d618d55
Hrishi Singh <hrishi0115@gmail.com>
2024-07-02 16:52:21 +0100

    improved OOP design and added new method that returns all visualisations dynamically

--- backtesting_engine/visualization.py
+++ backtesting_engine/visualization.py
@@ -4,11 +4,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import inspect
 
 class Visualizer:
-    @staticmethod
-    def plot_portfolio_value(portfolio: pd.DataFrame):
-        plt.figure(figsize=(14,7))
+    def __init__(self):
+        pass
+
+    def plot_portfolio_value(self, portfolio: pd.DataFrame):
+        plt.figure(figsize=(14, 7))
         plt.plot(portfolio.index, portfolio['total'], label='Portfolio Total')
         plt.xlabel('Date')
         plt.ylabel('Total Value')
@@ -16,22 +19,20 @@ class Visualizer:
         plt.legend()
         plt.grid(True)
         plt.show()
-    
-    @staticmethod
-    def plot_signals(signals: pd.DataFrame):
-        plt.figure(figsize=(14,7))
+
+    def plot_signals(self, signals: pd.DataFrame):
+        plt.figure(figsize=(14, 7))
         plt.plot(signals.index, signals['price'], color='black')
         plt.plot(signals.index, signals['short_mavg'], label='Short-term SMA', color='blue')
         plt.plot(signals.index, signals['long_mavg'], label='Long-term SMA', color='red')
 
-        # plot buy signals
-
+        # Plot buy signals
         buy_signals = signals[signals['positions'] == 1.0]
         plt.plot(buy_signals.index, buy_signals['price'], '^', markersize=10, color='g', label='Buy Signal', lw=0)
-        
-        # plot sell signals
+
+        # Plot sell signals
         sell_signals = signals[signals['positions'] == -1.0]
-        plt.plot(sell_signals.index, sell_signals['price'], '^', markersize=10, color='r', label='Sell Signal', lw=0)
+        plt.plot(sell_signals.index, sell_signals['price'], 'v', markersize=10, color='r', label='Sell Signal', lw=0)
 
         plt.xlabel('Date')
         plt.ylabel('Price')
@@ -40,6 +41,23 @@ class Visualizer:
         plt.grid(True)
         plt.show()
 
+    def all_visualizations(self, portfolio: pd.DataFrame, signals: pd.DataFrame):
+        methods = inspect.getmembers(self, predicate=inspect.ismethod)
+        plot_methods = [method for name, method in methods if name.startswith('plot_')]
+        for plot_method in plot_methods:
+            # Check the method signature to determine the appropriate argument
+            sig = inspect.signature(plot_method)
+            if 'portfolio' in sig.parameters:
+                plot_method(portfolio)
+            elif 'signals' in sig.parameters:
+                plot_method(signals)
+
+        # alternatively
+        # self.plot_portfolio_value(portfolio)
+        # self.plot_signals(signals)
+
+
+
 # Example usage
 if __name__ == "__main__":
     from data_loader import YahooFinanceLoader
@@ -47,9 +65,9 @@ if __name__ == "__main__":
     from simulation_engine import SimulationEngine
 
     loader = YahooFinanceLoader()
-    data = loader.load_data('NVDA', '2020-01-01', '2022-01-01')
+    data = loader.load_data('AAPL', '2020-01-01', '2024-07-01')
 
-    strategy = MovingAverageCrossoverStrategy(40, 100)
+    strategy = MovingAverageCrossoverStrategy(40, 80)
 
     signals = strategy.generate_signals(data)
 
@@ -57,11 +75,11 @@ if __name__ == "__main__":
 
     portfolio = engine.simulate_trades(signals)
 
-    mask = signals['positions'] != 0.0
-    print(signals[mask])
-    print(portfolio)
+    # mask = signals['positions'] != 0.0
+    # print(signals[mask])
+    # print(portfolio)
 
     visualizer = Visualizer()
-    visualizer.plot_portfolio_value(portfolio)
-    visualizer.plot_signals(signals)
-
+    # visualizer.plot_portfolio_value(portfolio)
+    # visualizer.plot_signals(signals)
+    visualizer.all_visualizations(portfolio, signals)
